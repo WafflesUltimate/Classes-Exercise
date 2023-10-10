62f7fa5d47380b49a1655ccac9dfb691fa4a9b3b
David Armstrong <wafflesultimate1@gmail.com>
2023-10-10 09:42:08 -0700

    completed Exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,40 @@
 ﻿namespace Classes
 {
+
+    public class Cars
+    {
+        public string Make { get; set;}
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public void PrintCarDetails() { Console.WriteLine($"The car is a {Make} {Model} made in {Year}.");}
+
+    }
+
+
+
+
+
     public class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Cars aCar = new Cars();
+
+            aCar.Make = "Honda";
+
+            aCar.Model = "Civic";
+
+            aCar.Year = 2023;
+
+            aCar.PrintCarDetails();
+
         }
     }
 }
